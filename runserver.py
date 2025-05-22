e7297200ea95b0bdd22c4175a95d3e65ef055e02
gamerfan82 <83762731+gamerfan82@users.noreply.github.com>
2025-05-22 17:07:12 +0330

    Update runserver.py
    del pythonanywhere

--- runserver.py
+++ runserver.py
@@ -1,19 +1,6 @@
-import requests
 from flask import Flask, render_template_string
 
-username = 'gamerfan82'
-token = 'df0617391acfbb4d0f3ef9130487d8c7b4d7b23e'
-#/api/v0/user/{username}/consoles/{id}/get_latest_output/
-response = requests.post(
-    'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/{id}/send_input/'.format(
-        username=username,
-        id="39795363"
-    ),
-    headers={'Authorization': 'Token {token}'.format(token=token)},
-    json={"input": "python3.11 .pythonstartup.py\n"}
-)
-#pip install -r requirements.txt
-def runweb():    
+def runsite():    
     app = Flask(__name__)
     
     html_content = """
@@ -71,9 +58,3 @@ def runweb():
     if __name__ == '__main__':
         app.run(debug=True,port=8080)
     
-
-if response.status_code == 200:
-    print("Command sent successfully. : "+ response.text)
-    runweb()
-else:
-    print(f"Error {response.status_code}: {response.text}")
