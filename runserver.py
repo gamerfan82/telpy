a29f992f244c6b675c12213bcd5f192b55ce75b6
gamerfan82 <83762731+gamerfan82@users.noreply.github.com>
2025-05-22 19:19:50 +0330

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -56,6 +56,6 @@ def runsite():
         return render_template_string(html_content)
     
     if __name__ == '__main__':
-        app.run(debug=True,port=8080)
+        app.run(debug=True, host='0.0.0.0', port=8080)
 runsite()
     
